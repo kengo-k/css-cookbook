5c93ca9e0e8de0db20bf4cdcf96a1dd19761246f
kengo-k <31839234+kengo-k@users.noreply.github.com>
2023-07-24 20:33:54 +0900

    add hello page with tailwindcss

---
+++ src/pages/hello.js
@@ -0,0 +1,13 @@
+import "../app/globals.css";
+
+function Hello() {
+  return (
+    <div className="flex justify-center items-center h-screen bg-gray-100">
+      <button className="px-4 py-2 bg-blue-500 text-white rounded hover:bg-blue-700">
+        Hello World!
+      </button>
+    </div>
+  );
+}
+
+export default Hello;
