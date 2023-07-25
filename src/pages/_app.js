7f9709d3dc394228b5429ae2c67c3dde308aa165
kengo-k <31839234+kengo-k@users.noreply.github.com>
2023-07-25 18:14:22 +0900

    add _app.js: global pages layout

---
+++ src/pages/_app.js
@@ -0,0 +1,17 @@
+import "../app/globals.css";
+
+export default function MyApp({ Component, pageProps }) {
+  return (
+    <div>
+      <header>
+        {/* This is the header component. You can use it to display common elements like navigation menus at the top of each page. */}
+      </header>
+      <main>
+        <Component {...pageProps} />
+      </main>
+      <footer>
+        {/* // This is the footer component. You can use it to display common elements at the bottom of each page, such as copyright notices or links. */}
+      </footer>
+    </div>
+  );
+}
