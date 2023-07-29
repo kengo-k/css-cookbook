34299ad4c0b5bc590e9a6d77e2cf3392351b013b
kengo-k <31839234+kengo-k@users.noreply.github.com>
2023-07-29 12:22:46 +0900

    enable prismjs line-numbers

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,6 +5,8 @@ import { andada_pro } from "@/utils/fonts";
 import "prism-themes/themes/prism-ghcolors.css";
 import Prism from "prismjs";
 import "prismjs/components/prism-css.min.js";
+import "prismjs/plugins/line-numbers/prism-line-numbers.css";
+import "prismjs/plugins/line-numbers/prism-line-numbers.js";
 import { useEffect } from "react";
 
 const MarginExsampleGoal = createShowcaseItem({
@@ -159,6 +161,49 @@ const FlexExample2 = createShowcaseItem({
   lang: "html",
 });
 
+const FlexExample3 = createShowcaseItem({
+  Component: () => {
+    return (
+      //<div className="mx-auto flex flex-wrap p-5 flex-col md:flex-row items-center">
+      <div className="flex flex-row items-center bg-gray-200">
+        {/* <a className="flex title-font font-medium items-center text-gray-900 mb-4 md:mb-0"> */}
+        <a>
+          <span className="ml-3 text-xl">My Awesome Site</span>
+        </a>
+        <nav className="flex flex-row items-center justify-center text-base py-4 h-32 ml-auto">
+          <a className="border-2 border-gray-500 p-2 ml-5 hover:text-gray-900">
+            Link1
+          </a>
+          <a className="border-2 border-gray-500 p-2 ml-5 hover:text-gray-900">
+            Link2
+          </a>
+          <a className="border-2 border-gray-500 p-2 ml-5 hover:text-gray-900">
+            Link3
+          </a>
+          <a className="border-2 border-gray-500 p-2 ml-5 hover:text-gray-900">
+            Link4
+          </a>
+        </nav>
+      </div>
+    );
+  },
+  code: `<nav className="flex flex-row items-center justify-center text-base bg-gray-200 py-4 h-32">
+  <a className="border-2 border-gray-500 p-2 ml-5 hover:text-gray-900">
+    Link1
+  </a>
+  <a className="border-2 border-gray-500 p-2 ml-5 hover:text-gray-900">
+    Link2
+  </a>
+  <a className="border-2 border-gray-500 p-2 ml-5 hover:text-gray-900">
+    Link3
+  </a>
+  <a className="border-2 border-gray-500 p-2 ml-5 hover:text-gray-900">
+    Link4
+  </a>
+</nav>`,
+  lang: "html",
+});
+
 export default function Home() {
   useEffect(() => {
     Prism.highlightAll();
@@ -219,7 +264,11 @@ export default function Home() {
         </div>
 
         <div>
-          <Showcase title={"Step4: Align to left"} item={MarginSample4} />
+          <Showcase
+            title={"Step4: Align to left"}
+            item={MarginSample4}
+            showCode={true}
+          />
           <article className="mt-6">
             By specifying ml-auto, the area occupied by the element is
             right-aligned.
@@ -278,6 +327,17 @@ export default function Home() {
               </li>
             </ul>
           </article>
+        </div>
+      </section>
+
+      <section>
+        <div>
+          <Showcase
+            title={"Step3: Align the navigation links to the right"}
+            item={FlexExample3}
+            showCode={true}
+          />
+          <article className="mt-6">todo</article>
         </div>
       </section>
     </main>
--- src/components/common/HighlightedSource.tsx
+++ src/components/common/HighlightedSource.tsx
@@ -1,6 +1,6 @@
 export function HighlightedSource(props) {
   return (
-    <pre>
+    <pre className="line-numbers" style={{ overflow: "auto" }}>
       <code className={`language-${props.lang}`}>{props.code}</code>
     </pre>
   );
