5c81007419bd822bbe8a4730e7ff50bc47e111eb
kengo-k <31839234+kengo-k@users.noreply.github.com>
2023-07-30 22:54:28 +0900

    HighlightedSource: add type definition

--- src/components/common/HighlightedSource.tsx
+++ src/components/common/HighlightedSource.tsx
@@ -1,7 +1,12 @@
-export function HighlightedSource(props) {
+export interface Props {
+  lang: string;
+  code: string;
+}
+
+export const HighlightedSource = (props: Props) => {
   return (
     <pre className="line-numbers" style={{ overflow: "auto" }}>
       <code className={`language-${props.lang}`}>{props.code}</code>
     </pre>
   );
-}
+};
